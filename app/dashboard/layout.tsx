a8f9c89010641dbbd8eea61d47bc435120fce6a4
opeyemi-jonah <gavrieljonah@gmail.com>
2025-06-17 17:51:34 -0700

    Search component enhanced with debounce technique: 500ms

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,4 +1,6 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
+
+export const experimental_ppr = true; // Enable Partial Page Rendering (PPR) for this layout
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
